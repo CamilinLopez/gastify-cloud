1967758cb537a88921834f9e640b79abfd3855b0
RaulDiaz <rulhdiaz@gmail.com>
2024-06-05 15:18:48 -0600

    commit

--- frontend/components/inicio/index.tsx
+++ frontend/components/inicio/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Resumen_ventas from './resumen_ventas';
+import Notificaciones_alertas from './notificaciones_alertas';
 
 export default function SectionsInicio() {
   return (
     <div className='w-full' >
+      <Notificaciones_alertas />
       <Resumen_ventas />
     </div>
   );
--- frontend/components/inicio/notificaciones_alertas.tsx
+++ frontend/components/inicio/notificaciones_alertas.tsx
@@ -8,8 +8,188 @@ import React from 'react'
 
 export default function Notificaciones_alertas() {
   return (
-    <div>
-      
+    <>
+    <div className='bg-blanco px-4 py-6 w-full'>
+
+    
+      <div className="">
+      <div className="flex flex-col w-[204.73px] h-[22.5px] items-start relative">
+        <div className="flex flex-col items-start relative self-stretch w-full flex-[0_0_auto]">
+          <div className="relative w-fit mt-[-1.00px] [font-family:'Inter-Bold',Helvetica] font-bold text-[#111416] text-lg tracking-[-0.27px] leading-[22.5px] whitespace-nowrap">
+            Notificaciones y Alertas
+          </div>
+        </div>
+      </div>
     </div>
+    <div className="bg-blanco px-4 py-6 w-full">
+      <div className="flex flex-col w-[928px] h-[407.5px] items-start relative bg-white rounded-xl overflow-hidden border border-solid border-[#dbe0e5]">
+        <div className="flex flex-col w-[926px] h-[405.5px] items-start relative">
+          <div className="h-[45.5px] flex flex-col w-[926px] items-start relative">
+            <div className="flex w-[926px] h-[45.5px] items-start relative bg-white">
+              <div className="flex flex-col w-[352.94px] h-[45.5px] items-start px-4 py-3 relative">
+                <div className="flex flex-col w-[320.94px] h-[21px] items-start relative">
+                  <div className="relative self-stretch mt-[-1.00px] [font-family:'Inter-Medium',Helvetica] font-medium text-[#111416] text-sm tracking-[0] leading-[21px]">
+                    Tipo de cilindro
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-col w-[351.34px] h-[45.5px] items-start px-4 py-3 relative">
+                <div className="flex-col w-[319.34px] h-[21px] items-start flex relative">
+                  <div className="relative self-stretch mt-[-1.00px] [font-family:'Inter-Medium',Helvetica] font-medium text-[#111416] text-sm tracking-[0] leading-[21px]">
+                    Stock
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-col w-[221.72px] h-[45.5px] items-start px-4 py-3 relative">
+                <div className="flex-col w-[189.72px] h-[21px] items-start flex relative">
+                  <div className="relative self-stretch mt-[-1.00px] [font-family:'Inter-Medium',Helvetica] font-medium text-[#111416] text-sm tracking-[0] leading-[21px]">
+                    Prioridad
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="h-[360px] flex flex-col w-[926px] items-start relative">
+            <div className="flex w-[926px] h-[72px] items-start relative border-t [border-top-style:solid] border-[#e5e8ea]">
+              <div className="w-[352.94px] flex flex-col h-[72px] items-center justify-center px-4 py-2 relative">
+                <div className="flex flex-col w-[320.94px] h-[21px] items-start relative">
+                  <div className="relative self-stretch mt-[-1.00px] [font-family:'Inter-Regular',Helvetica] font-normal text-[#111416] text-sm tracking-[0] leading-[21px]">
+                    5 kg
+                  </div>
+                </div>
+              </div>
+              <div className="w-[351.34px] flex flex-col h-[72px] items-center justify-center px-4 py-2 relative">
+                <div className="flex-col w-[319.34px] h-[21px] items-start flex relative">
+                  <div className="relative self-stretch mt-[-1.00px] [font-family:'Inter-Regular',Helvetica] font-normal text-[#637787] text-sm tracking-[0] leading-[21px]">
+                    200
+                  </div>
+                </div>
+              </div>
+              <div className="w-[221.72px] flex flex-col h-[72px] items-center justify-center px-4 py-2 relative">
+                <div className="w-[189.72px] h-8 items-center justify-center px-4 py-0 bg-[#eff2f4] rounded-xl overflow-hidden flex relative">
+                  <div className="flex flex-col w-[26.52px] h-[21px] items-start relative bg-[#eff2f4]">
+                    <div className="flex flex-col items-start relative self-stretch w-full flex-[0_0_auto]">
+                      <div className="relative w-fit mt-[-1.00px] [font-family:'Inter-Medium',Helvetica] font-medium text-[#111416] text-sm tracking-[0] leading-[21px] whitespace-nowrap">
+                        Alta
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="flex w-[926px] h-[72px] items-start relative border-t [border-top-style:solid] border-[#e5e8ea]">
+              <div className="w-[352.94px] flex flex-col h-[72px] items-center justify-center px-4 py-2 relative">
+                <div className="flex flex-col w-[320.94px] h-[21px] items-start relative">
+                  <div className="relative self-stretch mt-[-1.00px] [font-family:'Inter-Regular',Helvetica] font-normal text-[#111416] text-sm tracking-[0] leading-[21px]">
+                    11 kg
+                  </div>
+                </div>
+              </div>
+              <div className="w-[351.34px] flex flex-col h-[72px] items-center justify-center px-4 py-2 relative">
+                <div className="flex-col w-[319.34px] h-[21px] items-start flex relative">
+                  <div className="relative self-stretch mt-[-1.00px] [font-family:'Inter-Regular',Helvetica] font-normal text-[#637787] text-sm tracking-[0] leading-[21px]">
+                    300
+                  </div>
+                </div>
+              </div>
+              <div className="w-[221.72px] flex flex-col h-[72px] items-center justify-center px-4 py-2 relative">
+                <div className="w-[189.72px] h-8 items-center justify-center px-4 py-0 bg-[#eff2f4] rounded-xl overflow-hidden flex relative">
+                  <div className="w-[41.06px] flex flex-col h-[21px] items-start relative bg-[#eff2f4]">
+                    <div className="flex flex-col items-start relative self-stretch w-full flex-[0_0_auto]">
+                      <div className="relative w-fit mt-[-1.00px] [font-family:'Inter-Medium',Helvetica] font-medium text-[#111416] text-sm tracking-[0] leading-[21px] whitespace-nowrap">
+                        Media
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="flex w-[926px] h-[72px] items-start relative border-t [border-top-style:solid] border-[#e5e8ea]">
+              <div className="w-[352.94px] flex flex-col h-[72px] items-center justify-center px-4 py-2 relative">
+                <div className="flex flex-col w-[320.94px] h-[21px] items-start relative">
+                  <div className="relative self-stretch mt-[-1.00px] [font-family:'Inter-Regular',Helvetica] font-normal text-[#111416] text-sm tracking-[0] leading-[21px]">
+                    15 kg
+                  </div>
+                </div>
+              </div>
+              <div className="w-[351.34px] flex flex-col h-[72px] items-center justify-center px-4 py-2 relative">
+                <div className="flex-col w-[319.34px] h-[21px] items-start flex relative">
+                  <div className="relative self-stretch mt-[-1.00px] [font-family:'Inter-Regular',Helvetica] font-normal text-[#637787] text-sm tracking-[0] leading-[21px]">
+                    400
+                  </div>
+                </div>
+              </div>
+              <div className="w-[221.72px] flex flex-col h-[72px] items-center justify-center px-4 py-2 relative">
+                <div className="w-[189.72px] h-8 items-center justify-center px-4 py-0 bg-[#eff2f4] rounded-xl overflow-hidden flex relative">
+                  <div className="w-[28.64px] flex flex-col h-[21px] items-start relative bg-[#eff2f4]">
+                    <div className="flex flex-col items-start relative self-stretch w-full flex-[0_0_auto]">
+                      <div className="relative w-fit mt-[-1.00px] [font-family:'Inter-Medium',Helvetica] font-medium text-[#111416] text-sm tracking-[0] leading-[21px] whitespace-nowrap">
+                        Baja
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="flex w-[926px] h-[72px] items-start relative border-t [border-top-style:solid] border-[#e5e8ea]">
+              <div className="w-[352.94px] flex flex-col h-[72px] items-center justify-center px-4 py-2 relative">
+                <div className="flex flex-col w-[320.94px] h-[21px] items-start relative">
+                  <div className="relative self-stretch mt-[-1.00px] [font-family:'Inter-Regular',Helvetica] font-normal text-[#111416] text-sm tracking-[0] leading-[21px]">
+                    45 kg
+                  </div>
+                </div>
+              </div>
+              <div className="w-[351.34px] flex flex-col h-[72px] items-center justify-center px-4 py-2 relative">
+                <div className="flex-col w-[319.34px] h-[21px] items-start flex relative">
+                  <div className="relative self-stretch mt-[-1.00px] [font-family:'Inter-Regular',Helvetica] font-normal text-[#637787] text-sm tracking-[0] leading-[21px]">
+                    500
+                  </div>
+                </div>
+              </div>
+              <div className="w-[221.72px] flex flex-col h-[72px] items-center justify-center px-4 py-2 relative">
+                <div className="w-[189.72px] h-8 items-center justify-center px-4 py-0 bg-[#eff2f4] rounded-xl overflow-hidden flex relative">
+                  <div className="flex flex-col w-[26.52px] h-[21px] items-start relative bg-[#eff2f4]">
+                    <div className="flex flex-col items-start relative self-stretch w-full flex-[0_0_auto]">
+                      <div className="relative w-fit mt-[-1.00px] [font-family:'Inter-Medium',Helvetica] font-medium text-[#111416] text-sm tracking-[0] leading-[21px] whitespace-nowrap">
+                        Alta
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="flex w-[926px] h-[72px] items-start relative border-t [border-top-style:solid] border-[#e5e8ea]">
+              <div className="w-[352.94px] flex flex-col h-[72px] items-center justify-center px-4 py-2 relative">
+                <div className="flex flex-col w-[320.94px] h-[21px] items-start relative">
+                  <div className="relative self-stretch mt-[-1.00px] [font-family:'Inter-Regular',Helvetica] font-normal text-[#111416] text-sm tracking-[0] leading-[21px]">
+                    H15
+                  </div>
+                </div>
+              </div>
+              <div className="w-[351.34px] flex flex-col h-[72px] items-center justify-center px-4 py-2 relative">
+                <div className="flex-col w-[319.34px] h-[21px] items-start flex relative">
+                  <div className="relative self-stretch mt-[-1.00px] [font-family:'Inter-Regular',Helvetica] font-normal text-[#637787] text-sm tracking-[0] leading-[21px]">
+                    600
+                  </div>
+                </div>
+              </div>
+              <div className="w-[221.72px] flex flex-col h-[72px] items-center justify-center px-4 py-2 relative">
+                <div className="w-[189.72px] h-8 items-center justify-center px-4 py-0 bg-[#eff2f4] rounded-xl overflow-hidden flex relative">
+                  <div className="w-[41.06px] flex flex-col h-[21px] items-start relative bg-[#eff2f4]">
+                    <div className="flex flex-col items-start relative self-stretch w-full flex-[0_0_auto]">
+                      <div className="relative w-fit mt-[-1.00px] [font-family:'Inter-Medium',Helvetica] font-medium text-[#111416] text-sm tracking-[0] leading-[21px] whitespace-nowrap">
+                        Media
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+    </>
   )
 }
