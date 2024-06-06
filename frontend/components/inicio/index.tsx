3a3c76c8232c586bfd6390bd01c970409cb450f5
CamilinLopez <camilollc98@gmail.com>
2024-06-06 11:14:51 -0500

    add rendimiento conductores section

--- frontend/components/inicio/index.tsx
+++ frontend/components/inicio/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Resumen_ventas from './resumen_ventas';
+import Rendimiento_conductores from './rendimiento_conductores';
 
 export default function SectionsInicio() {
   return (
-    <div className='w-full' >
+    <div className="w-full">
       <Resumen_ventas />
+      <Rendimiento_conductores />
+      
     </div>
   );
 }
