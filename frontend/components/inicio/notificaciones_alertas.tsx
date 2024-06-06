642fa05210f85671b214f9b54cc2fc53cfaefbb5
ruldiaz <rulhdiaz@gmail.com>
2024-06-05 20:41:51 -0600

    commit

--- frontend/components/inicio/notificaciones_alertas.tsx
+++ frontend/components/inicio/notificaciones_alertas.tsx
@@ -6,35 +6,35 @@ export default function Notificaciones_alertas() {
       <div className='bg-blanco px-4 py-6 w-full'>
         <h1 className='text-18px'>Notificaciones y Alertas</h1>
       </div>
-      <div className='table-general'>
-        <table>
+      <div className='w-full rounded-lg border'>
+        <table className='table-border w-full'>
             <thead>
-              <th>Tipo de cilindro</th>
-              <th>Stock</th>
-              <th>Prioridad</th>
+              <th className='table-header'>Tipo de cilindro</th>
+              <th className='table-header'>Stock</th>
+              <th className='table-header'>Prioridad</th>
             </thead>
             <tbody>
-              <tr>
+              <tr className='table-border'>
                 <td className='table-data left-table-data'>5 kg</td>
                 <td className='table-data center-table-data'>200</td>
                 <td className='table-data right-table-data'>Alta</td>
               </tr>
-              <tr>
+              <tr className='table-border'>
                 <td className='table-data left-table-data'>11 kg</td>
                 <td className='table-data center-table-data'>300</td>
                 <td className='table-data right-table-data'>Media</td>
               </tr>
-              <tr>
+              <tr className='table-border'>
                 <td className='table-data left-table-data'>15 kg</td>
                 <td className='table-data center-table-data'>400</td>
                 <td className='table-data right-table-data'>Baja</td>
               </tr>
-              <tr>
+              <tr className='table-border'>
                 <td className='table-data left-table-data'>45 kg</td>
                 <td className='table-data center-table-data'>500</td>
                 <td className='table-data right-table-data'>Alta</td>
               </tr>
-              <tr>
+              <tr className='table-border'>
                 <td className='table-data left-table-data'>H15</td>
                 <td className='table-data center-table-data'>600</td>
                 <td className='table-data right-table-data'>Media</td>
