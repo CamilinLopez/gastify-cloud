e71e4b73fd166786e9f2feaafea8a55df7112d00
ruldiaz <rulhdiaz@gmail.com>
2024-06-06 19:06:58 -0600

    commit

--- frontend/components/inicio/index.tsx
+++ frontend/components/inicio/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Resumen_ventas from './resumen_ventas';
 import Rendimiento_conductores from './rendimiento_conductores';
 import ResumenInventario from './resumen_inventario';
+import Notificaciones_alertas from './notificaciones_alertas';
 
 export default function SectionsInicio() {
   return (
     <div className="w-full">
       <ResumenInventario />
+      <Notificaciones_alertas />
       <Resumen_ventas />
       <Rendimiento_conductores />
     </div>
--- frontend/components/inicio/notificaciones_alertas.tsx
+++ frontend/components/inicio/notificaciones_alertas.tsx
@@ -1,46 +1,44 @@
 import React from 'react'
 
 export default function Notificaciones_alertas() {
+
+  const headers = ['Tipo de cilindro','Stock','Prioridad'];
+
+  const data = [
+    {tipo_cilindro: 5, stock: 200, prioridad: 'alta'},
+    {tipo_cilindro: 11, stock: 300, prioridad: 'media'},
+    {tipo_cilindro: 15, stock: 400, prioridad: 'baja'},
+    {tipo_cilindro: 45, stock: 500, prioridad: 'alta'},
+    {tipo_cilindro: 'H15', stock: 600, prioridad: 'media'}
+  ];
+
+  
   return (
     <>
       <div className='bg-blanco px-4 py-6 w-full'>
         <h1 className='text-18px'>Notificaciones y Alertas</h1>
       </div>
-      <div className='w-full rounded-lg border'>
-        <table className='table-border w-full'>
-            <thead>
-              <th className='table-header'>Tipo de cilindro</th>
-              <th className='table-header'>Stock</th>
-              <th className='table-header'>Prioridad</th>
+      <div className='px-4'>
+        <div className='rounded-lg overflow-hidden border border-gray-300'>
+          <table className='min-w-full border-collapse'>
+            <thead className='bg-blanco'>
+              <tr>
+                {headers.map((head, index) => (
+                  <th key={index} className='font-medium text-15px text-left border-t border-b border-gray-300 px-4 py-4'>{head}</th>
+                ))}
+              </tr>
             </thead>
             <tbody>
-              <tr className='table-border'>
-                <td className='table-data left-table-data'>5 kg</td>
-                <td className='table-data center-table-data'>200</td>
-                <td className='table-data right-table-data'>Alta</td>
-              </tr>
-              <tr className='table-border'>
-                <td className='table-data left-table-data'>11 kg</td>
-                <td className='table-data center-table-data'>300</td>
-                <td className='table-data right-table-data'>Media</td>
-              </tr>
-              <tr className='table-border'>
-                <td className='table-data left-table-data'>15 kg</td>
-                <td className='table-data center-table-data'>400</td>
-                <td className='table-data right-table-data'>Baja</td>
-              </tr>
-              <tr className='table-border'>
-                <td className='table-data left-table-data'>45 kg</td>
-                <td className='table-data center-table-data'>500</td>
-                <td className='table-data right-table-data'>Alta</td>
-              </tr>
-              <tr className='table-border'>
-                <td className='table-data left-table-data'>H15</td>
-                <td className='table-data center-table-data'>600</td>
-                <td className='table-data right-table-data'>Media</td>
-              </tr>
+              {data.map((row, index) => (
+                <tr key={index} className='border-b border-gray-300'>
+                  <td className='text-14px px-4 py-4 w-4/12'>{row.tipo_cilindro} kg</td>
+                  <td className='font-thin non-italic text-secondary-14px px-4 py-4 w-4/12 text-gray-400'>{row.stock}</td>
+                  <td className='text-14px px-4 py-4'><p className='rounded-lg border border-color: #F0F2F5 text-center w-8/12 px-4 py-1 bg-[#F0F2F5]'>{row.prioridad.charAt(0).toUpperCase() + row.prioridad.substring(1)}</p></td>
+                </tr>
+              ))}
             </tbody>
           </table>
+        </div>
       </div>
     </>
   )
