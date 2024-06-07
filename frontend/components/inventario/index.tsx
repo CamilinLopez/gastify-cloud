94e4b3e6ad812c99c5db7e6268f8dfba5f0f1278
CamilinLopez <camilollc98@gmail.com>
2024-06-08 01:10:20 +0700

    all merge sections

--- frontend/components/inventario/index.tsx
+++ frontend/components/inventario/index.tsx
@@ -1,8 +1,10 @@
 import MovimientosDiarios from './movimientos_diarios';
+import Registro_entradas_salidas from './registro_entradas_salidas';
 
 export default function SectionsInventario() {
   return (
     <div className="w-full">
+      <Registro_entradas_salidas />
       <MovimientosDiarios />
     </div>
   );
---
+++ frontend/components/inventario/registro_entradas_salidas.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Registro_entradas_salidas() {
+  return (
+    <div className="w-full">
+      <div>
+        <h1 className="text-18px">Registro de entradas y salidas</h1>
+      </div>
+    </div>
+  );
+}
