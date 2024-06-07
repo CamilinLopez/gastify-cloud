569cb5cb1e2c8ac773aa74cfcd8f29464b781006
Felipe Arce <arceprogramando@gmail.com>
2024-06-07 00:35:07 -0300

    feat: initiate "movimientos_diarios"

---
+++ frontend/types/movimientos_diarios.ts
@@ -0,0 +1,9 @@
+export interface InfoMovimientosDiarios {
+  tipoCilindro: string;
+  Llenos: number;
+  Vacios: number;
+  Fallados: number;
+  Prestados: number;
+  FechadeMovimientos: string;
+  IDResponsable: number;
+}
