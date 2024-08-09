a312b90057f4dd058e0c17ea241724219035c49c
CamilinLopez <camilollc98@gmail.com>
2024-08-09 11:55:59 -0500

    merge camiloLopez feature/empresas

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -3,36 +3,36 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 
-export async function middleware(request: NextRequest) {
-  const jwt = request.cookies.get('token');
+// export async function middleware(request: NextRequest) {
+//   const jwt = request.cookies.get('token');
 
-  if (!jwt) {
-    return NextResponse.redirect(new URL('/signin', request.url));
-  }
-console.log(jwt)
-  try {
+//   if (!jwt) {
+//     return NextResponse.redirect(new URL('/signin', request.url));
+//   }
+// console.log(jwt)
+//   try {
 
-    const response = await fetch('https://localhost:3001/empresa/verificar-token', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ token: jwt.value }),
-    });
+//     const response = await fetch('https://localhost:3001/empresa/verificar-token', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({ token: jwt.value }),
+//     });
 
-    const data = await response.json();
+//     const data = await response.json();
 
-    if (!data.valid) {
-      throw new Error('Token inválido');
-    }
+//     if (!data.valid) {
+//       throw new Error('Token inválido');
+//     }
 
-    return NextResponse.next();
-  } catch (error) {
-    console.error('Error verifying JWT token:', error);
-    return NextResponse.redirect(new URL('/signin', request.url));
-  }
-}
+//     return NextResponse.next();
+//   } catch (error) {
+//     console.error('Error verifying JWT token:', error);
+//     return NextResponse.redirect(new URL('/signin', request.url));
+//   }
+// }
 
-export const config = {
-  matcher: ['/dashboard/:path*', '/', '/((?!registrar|signin).*)'],
-};
+// export const config = {
+//   matcher: ['/dashboard/:path*', '/', '/((?!registrar|signin).*)'],
+// };
