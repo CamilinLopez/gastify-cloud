c3b20aa8f9271bfc4f24a542f15451f0fb08e01a
CamilinLopez <camilollc98@gmail.com>
2024-08-18 22:17:50 -0500

    add new cookie 6

--- backend/src/handlers/empresaHandler.js
+++ backend/src/handlers/empresaHandler.js
@@ -41,7 +41,7 @@ const signinEmpresa = async (req, res, next) => {
       req.login(user, { session: false }, async (err) => {
         if (err) return next(err);
         const body = { id: user };
-        console.log(user.dataValues.id, 'papapapa', user.id, 'quesosososo', user.empresas, 'mamaa');
+        console.log(user.dataValues, 'papapapa', user.id, 'quesosososo', user.empresas, 'mamaa');
 
         const token = jwt.sign(body, SECRET_KEY);
 
