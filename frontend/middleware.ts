626068ff444e493655f288bbd3c85895b605b29a
CamilinLopez <camilollc98@gmail.com>
2024-09-02 18:32:05 -0500

    add middleware

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,41 +1,41 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import Cookies from 'js-cookie';
 
 export async function middleware(request: NextRequest) {
   const jwt = request.cookies.get('token');
-  // try {
-  //   if (request.nextUrl.pathname.startsWith('/dashboard')) {
-  //     if (!jwt) return NextResponse.redirect(new URL('/signin', request.url));
-  //     const response = await fetch('http://localhost:3001/empresa/verificar-token', {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //       body: JSON.stringify({ token: jwt.value }),
-  //     });
+  try {
+    if (request.nextUrl.pathname.startsWith('/dashboard')) {
+      if (!jwt) return NextResponse.redirect(new URL('/signin', request.url));
+      const response = await fetch('http://localhost:3001/empresa/verificar-token', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ token: jwt.value }),
+      });
 
-  //     const data = await response.json();
+      const data = await response.json();
 
-  //     if (response.status == 200) {
-  //       Cookies.set('token', data.token, {
-  //         expires: 7, // La cookie expirará en 7 días
-  //         secure: true, // Solo en HTTPS defaul true
-  //         sameSite: 'none', // Misma política de sitio
-  //       });
+      if (response.status == 200) {
+        Cookies.set('token', data.token, {
+          expires: 7, // La cookie expirará en 7 días
+          secure: true, // Solo en HTTPS defaul true
+          sameSite: 'none', // Misma política de sitio
+        });
 
-  //       return NextResponse.next();
-  //     }
-  //     Cookies.remove('token');
+        return NextResponse.next();
+      }
+      Cookies.remove('token');
 
-  //     return NextResponse.redirect(new URL('/signin', request.url));
-  //   }
-  // } catch (error) {
-  //   Cookies.remove('token');
+      return NextResponse.redirect(new URL('/signin', request.url));
+    }
+  } catch (error) {
+    Cookies.remove('token');
 
-  //   return NextResponse.redirect(new URL('/signin', request.url));
-  // }
+    return NextResponse.redirect(new URL('/signin', request.url));
+  }
 }
-
 
 export const config = {
   matcher: ['/dashboard/:path*', '/', '/((?!registrar|signin|api|_next|public).*)'],
