3b1796cd9e277ff46d7c5d3b3615ae2c7c5e682e
CamilinLopez <camilollc98@gmail.com>
2024-10-10 14:06:13 -0500

    variables de entorno

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -4,11 +4,10 @@ require('dotenv/config')
 const WEBS_URL = process.env.WEBS_URL ;
 const SECRET_KEY = process.env.SECRET_KEY ;
 const PAGE_URL = process.env.PAGE_URL;
-const CORS_ORIGIN_URL = process.env.CORS_ORIGIN_URL;
 
 module.exports = {
   WEBS_URL,
   SECRET_KEY,
   PAGE_URL,
-  CORS_ORIGIN_URL
+  
 };
